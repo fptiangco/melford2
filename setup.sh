585f50c8930d0cb0fd2f7e22dc4ed2763aaf64f2
Francis Tiangco <fptiangco@gmail.com>
2026-01-24 15:27:25 +0000

    Update api and iac clone to public url

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-git clone git@github.com:fptiangco/melford2-api.git
-git clone git@github.com:fptiangco/melford2-iac.git
+git clone https://github.com/fptiangco/melford2-api.git
+git clone https://github.com/fptiangco/melford2-iac.git
